2631cbd9f64dcf6bbc5a4a99832708fa023d4ec4
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-22 14:23:46 +0530

    install cookie parser and wire up the routes to index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,14 +3,23 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 
 import drillsRouter from "./routes/drills.routes.js";
+import meRoutes from "./routes/me.js";
+import attemptRoutes from "./routes/attempts.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
+app.use(cookieParser());
 
 // connect DB
 connectDB();
 
+app.use(express.json());
+
 app.use("/api/drills", drillsRouter);
+app.use("/api", meRoutes);
+app.use("/api", attemptRoutes);
+
 
 app.use(express.json());
 app.get('/api/health', (req, res) => res.json({ ok: true }));
